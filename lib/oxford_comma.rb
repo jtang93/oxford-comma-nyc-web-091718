68d94d8fa7540092fc8bb84635451414fe3789d9
jtang93 <tang.jonathan93@gmail.com>
2018-09-13 01:18:59 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.size <= 2
     array.join(" and ")
   else
-    last_element = "and #{array.index(-1)}"
+    last_element = "and #{array[-1]}"
     array[-1].replace(last_element)
     array.join(", ")
   end
